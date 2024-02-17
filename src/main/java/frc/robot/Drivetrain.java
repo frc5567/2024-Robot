9e10e82618beb8abd399c187d90c15ffeb378440
JessicaLubke <emmaroverbeek@gmail.com>
2024-02-17 14:21:05 -0500

    Migrated ConfigPID to Phoenix 6.

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,17 +1,10 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
-import com.ctre.phoenix.motorcontrol.StatusFrame;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.sensors.PigeonIMU_StatusFrame;
-import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -86,6 +79,8 @@ public class Drivetrain {
         m_isDrivetrainForward = true;
 
         this.zeroSensors();
+
+        this.configPID();
     }
 
     /**
@@ -154,90 +149,41 @@ public class Drivetrain {
      */
     public void configPID() {
 
-        /* Configure the left Talon's selected sensor as integrated sensor */
-		m_leftConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //Local Feedback Source
-
-		/* Configure the Remote (Left) Talon's selected sensor as a remote sensor for the right Talon */
-		m_rightConfig.Feedback.FeedbackRemoteSensorID = m_leftLeader.getDeviceID(); //Device ID of Remote Source
-		m_rightConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder; // TalonFX_SelectedSensor; //Remote Source Type
-		
-		/* Now that the Left sensor can be used by the master Talon,
-		 * set up the Left (Aux) and Right (Master) distance into a single
-		 * Robot distance as the Master's Selected Sensor 0. */
-        //TalonFXInvertType _leftInvert = m_leftLeader.getInverted() ? TalonFXInvertType.Clockwise : TalonFXInvertType.CounterClockwise;
-        TalonFXInvertType _rightInvert = m_rightLeader.getInverted() ? TalonFXInvertType.Clockwise : TalonFXInvertType.CounterClockwise; 
-		setRobotDistanceConfigs(_rightInvert, m_rightConfig);
-
 		/* FPID for Distance */
-		m_rightConfig.Slot0.kV = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kF;
+		m_rightConfig.Slot0.kV = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kV;
 		m_rightConfig.Slot0.kP = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kP;
 		m_rightConfig.Slot0.kI = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kI;
 		m_rightConfig.Slot0.kD = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kD;
-		m_rightConfig.Slot0.integralZone = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kIzone;
-		m_rightConfig.Slot0.closedLoopPeakOutput = RobotMap.DrivetrainConstants.DISTANCE_GAINS.kPeakOutput;
 
-        m_rightConfig.motionCurveStrength = 4;
-
-		/** Heading Configs */
-		m_rightConfig.remoteFilter1.remoteSensorDeviceID = m_pigeon.getDeviceID();    //Pigeon Device ID
-		m_rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; //This is for a Pigeon over CAN
-		m_rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; //Set as the Aux Sensor
-		m_rightConfig.auxiliaryPID.selectedFeedbackCoefficient = 3600.0 / RobotMap.DrivetrainConstants.PIGEON_UNITS_PER_ROTATION; //Convert Yaw to tenths of a degree
-
-		/* false means talon's local output is PID0 + PID1, and other side Talon is PID0 - PID1
-		 *   This is typical when the master is the right Talon FX and using Pigeon
-		 * 
-		 * true means talon's local output is PID0 - PID1, and other side Talon is PID0 + PID1
-		 *   This is typical when the master is the left Talon FX and using Pigeon
-		 */
-		m_rightConfig.auxPIDPolarity = true;
+        //m_rightConfig.motionCurveStrength = 4;
 
 		/* FPID for Heading */
         //TODO: figure out if kV is the correct feedforward gain.
-		m_rightConfig.Slot1.kV = RobotMap.DrivetrainConstants.TURNING_GAINS.kF;
+		m_rightConfig.Slot1.kV = RobotMap.DrivetrainConstants.TURNING_GAINS.kV;
 		m_rightConfig.Slot1.kP = RobotMap.DrivetrainConstants.TURNING_GAINS.kP;
 		m_rightConfig.Slot1.kI = RobotMap.DrivetrainConstants.TURNING_GAINS.kI;
 		m_rightConfig.Slot1.kD = RobotMap.DrivetrainConstants.TURNING_GAINS.kD;
-		m_rightConfig.Slot1.integralZone = RobotMap.DrivetrainConstants.TURNING_GAINS.kIzone;
-		m_rightConfig.Slot1.closedLoopPeakOutput = RobotMap.DrivetrainConstants.TURNING_GAINS.kPeakOutput;
-
-
+		
 		/* Config the neutral deadband. */
-		m_leftConfig.neutralDeadband = RobotMap.DrivetrainConstants.NEUTRAL_DEADBAND;
-		m_rightConfig.neutralDeadband = RobotMap.DrivetrainConstants.NEUTRAL_DEADBAND;
-
-
-		/**
-		 * 1ms per loop.  PID loop can be slowed down if need be.
-		 * For example,
-		 * - if sensor updates are too slow
-		 * - sensor deltas are very small per update, so derivative error never gets large enough to be useful.
-		 * - sensor movement is very slow causing the derivative error to be near zero.
-		 */
-		int closedLoopTimeMs = 1;
-		m_rightLeader.configClosedLoopPeriod(0, closedLoopTimeMs, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_rightLeader.configClosedLoopPeriod(1, closedLoopTimeMs, RobotMap.DrivetrainConstants.TIMEOUT_MS);
+		//m_leftConfig.neutralDeadband = RobotMap.DrivetrainConstants.NEUTRAL_DEADBAND;
+		//m_rightConfig.neutralDeadband = RobotMap.DrivetrainConstants.NEUTRAL_DEADBAND;
 
 		/* Motion Magic Configs */
-		m_rightConfig.motionAcceleration = 9000; //(distance units per 100 ms) per second
-		m_rightConfig.motionCruiseVelocity = 12000; //distance units per 100 ms
+        var motionMagicConfigs = m_rightConfig.MotionMagic;
+		//m_rightConfig.motionAcceleration = 9000; //(distance units per 100 ms) per second
+		//m_rightConfig.motionCruiseVelocity = 12000; //distance units per 100 ms
+        motionMagicConfigs.MotionMagicCruiseVelocity = 12000; 
+        motionMagicConfigs.MotionMagicAcceleration = 9000;
 
 		/* APPLY the config settings */
-		m_leftLeader.configAllSettings(m_leftConfig);
-		m_rightLeader.configAllSettings(m_rightConfig);
-
-		/* Set status frame periods to ensure we don't have stale data */
-		/* These aren't configs (they're not persistant) so we can set these after the configs.  */
-		m_rightLeader.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_rightLeader.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 20, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_rightLeader.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 20, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_rightLeader.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_leftLeader.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5, RobotMap.DrivetrainConstants.TIMEOUT_MS);
-		m_pigeon.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR , 5, RobotMap.DrivetrainConstants.TIMEOUT_MS);
+		//m_leftLeader.configAllSettings(m_leftConfig);
+		//m_rightLeader.configAllSettings(m_rightConfig);
+        m_leftLeader.getConfigurator().apply(m_leftConfig);
+        m_rightLeader.getConfigurator().apply(m_rightConfig);
 
         /* Determine which slot affects which PID */
-        m_rightLeader.selectProfileSlot(0, RobotMap.DrivetrainConstants.PID_PRIMARY);
-        m_rightLeader.selectProfileSlot(1, RobotMap.DrivetrainConstants.PID_TURN);
+        //m_rightLeader.selectProfileSlot(0, RobotMap.DrivetrainConstants.PID_PRIMARY);
+        //m_rightLeader.selectProfileSlot(1, RobotMap.DrivetrainConstants.PID_TURN);
 
     }
 }
--- src/main/java/frc/robot/Gains.java
+++ src/main/java/frc/robot/Gains.java
@@ -8,7 +8,7 @@ public class Gains {
     public final double kP;
 	public final double kI;
 	public final double kD;
-	public final double kF;
+	public final double kV;
 	public final int kIzone;
 	public final double kPeakOutput;
 	
@@ -18,15 +18,15 @@ public class Gains {
      * @param _kP the proportionality constant. How much to change the value/power by
      * @param _kI the integral constant. Error between the current and desired value. Bumps the value/power to actually hit the target
      * @param _kD the derivative constant. The rate of change. It helps to dampen oscillation
-     * @param _kF the feed-forward constant. The minimum amount of power needed to overcome inertia (to actually get us to move). Use characterization to find this
+     * @param _kV the feed-forward constant. The minimum amount of power needed to overcome inertia (to actually get us to move). Use characterization to find this
      * @param _kIzone the intergral zone -> if absolute closed loop error exceeds this value, error is reset
      * @param _kPeakOutput the peak output of the PID control
 	 */
-	public Gains(double _kP, double _kI, double _kD, double _kF, int _kIzone, double _kPeakOutput){
+	public Gains(double _kP, double _kI, double _kD, double _kV, int _kIzone, double _kPeakOutput){
 		kP = _kP;
 		kI = _kI;
 		kD = _kD;
-		kF = _kF;
+		kV = _kV;
 		kIzone = _kIzone;
 		kPeakOutput = _kPeakOutput;
     }
